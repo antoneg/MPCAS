87ac5439a9b7f3c9c2cc0fe2ee914bda1c6ca53e
Anton EG <antogus@student.chalmers.se>
2020-11-11 21:13:31 +0100

    Removed some trash files

--- FFR120/HW1/ThirdTry/AlmostTrash/TrimPopulation.m
+++
@@ -1,9 +0,0 @@
-function trimmedPop = TrimPopulation(pop, stateDelim)
-    trimmedPop = [];
-    for i = 1:size(pop,1)
-        state = pop(i,5);
-        if state == stateDelim
-            trimmedPop(end+1,:) = pop(i,:);
-        end
-    end
-end
